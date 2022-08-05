d63b7547d304cf6ad356d1fc79280f8923d460ce
Isaac Barbosa <49215995+ProspektsMarch22@users.noreply.github.com>
2022-08-05 15:21:49 +0100

    Update kelvin.js
    Adds some coments in the original code.

--- kelvin.js
+++ kelvin.js
@@ -1,4 +1,4 @@
-//Kelvin Weather JavaScript
+//Kelvin Weather JavaScrip study
 
 //Current temperature in Kelvin. Must remain as a constant
 const kelvin = 298;
@@ -10,10 +10,11 @@ const celsius = kelvin - 273;
 let fahrenheit = celsius * (9/5) + 32
 fahrenheit = Math.floor(fahrenheit);
 
-//For extra practise, here comes the Newton scale
+//For extra practise, here comes the Newton scale. It also takes a formula to be calculated, and for reading sake i decided to go straight with a decimal value. So that we have to round up too.
 let newton = celsius * 0.33;
 newton = Math.floor(newton);
 
+//And with these console.logs we have effectively a kelvin translator to both fahrenheit and newton scales:
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 console.log(`The temperature is ${newton} degrees Newton.`);
